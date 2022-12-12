3b7612faf7c5f111907f9eef7bcae37ac95be32c
zhangshuangli <943165920@qq.com>
2022-12-12 20:29:52 +0800

    实现sku排列算法实现商品规格属性组demo

---
+++ sku排列算法实现商品规格属性组/data.js
@@ -0,0 +1,29 @@
+/*
+ * @Date: 2022-12-12 14:56:25
+ * @LastEditors: zhangshuangli
+ * @LastEditTime: 2022-12-12 20:02:01
+ * @Description: 这是****文件
+ */
+export default [
+  {
+    "name": "机型",
+    "list": [
+      { "name": "iPhone 13"},
+      { "name": "iPhone 14"}
+    ]
+  },
+  {
+    "name": "颜色",
+    "list": [
+      { "name": "蓝色"},
+      { "name": "白色"}
+    ]
+  },
+  {
+    "name": "存储容量",
+    "list": [
+      { "name": "64GB"},
+      { "name": "128GB"}
+    ]
+  }
+]
